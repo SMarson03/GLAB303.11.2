fb145c85f9dc56169b342535ced91ae8180a3f06
SMarson03 <167792197+SMarson03@users.noreply.github.com>
2024-09-18 15:00:15 -0400

    1

---
+++ src/main/java/org/example/CreateArrayListFromCollectionExample.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CreateArrayListFromCollectionExample{
+    public static void main(String[] args) {
+        List<Integer> fistFivePrimeNumbers = new ArrayList<>();
+        fistFivePrimeNumbers.add(2);
+        fistFivePrimeNumbers.add(3);
+        fistFivePrimeNumbers.add(5);
+        fistFivePrimeNumbers.add(7);
+        fistFivePrimeNumbers.add(11);
+
+        List<Integer> firstTenPrimerNumbers = new ArrayList<>(fistFivePrimeNumbers);
+
+        List<Integer> nextFiveNumbers = new ArrayList<>();
+        nextFiveNumbers.add(13);
+        nextFiveNumbers.add(17);
+        nextFiveNumbers.add(19);
+        nextFiveNumbers.add(23);
+        nextFiveNumbers.add(29);
+
+        firstTenPrimerNumbers.addAll(nextFiveNumbers);
+
+        System.out.println(firstTenPrimerNumbers);
+    }
+}
